c43cf74242d546377b7bc5a4e422fd8aee541727
sarman03 <sarmanpreets.it.22@nitj.ac.in>
2026-03-27 10:14:19 +0530

    gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { ImageOverrideProvider } from "@/components/ImageOverrideProvider";
 import "./globals.css";
 
@@ -79,6 +80,18 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full antialiased">
       <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-05XXE41TKY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-05XXE41TKY');
+          `}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
